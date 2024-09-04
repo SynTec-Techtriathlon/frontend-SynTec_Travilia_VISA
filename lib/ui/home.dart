43177aee84df28c6385f0ea0089c63ff87d73f9f
Rashmina Amarasinghe <131146632+Rashmina2000@users.noreply.github.com>
2024-09-04 22:45:09 +0530

    Home page

--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -11,10 +11,34 @@ class HomePage extends StatelessWidget {
         title: Text('Flutter Demo Home Page'),
       ),
       body: Center(
-        child: Column(children: [
-          Text('Home page', style: headingStyle),
-        ]),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              child: Image.asset('images/Emblem_of_Sri_Lanka.png'),
+              width: 200,
+            ),
+            SizedBox(
+              height: 50,
+            ),
+            MaterialButton(
+              onPressed: () {},
+              color: Colors.black,
+              child: Text(
+                'Apply Now',
+                style: TextStyle(
+                  fontSize: 22,
+                ),
+              ),
+              height: 50,
+              textColor: Colors.orange,
+              elevation: 5.0,
+            )
+            // Image(image: AssetImage('images/Emblem_of_Sri_Lanka.png'))
+          ],
+        ),
       ),
     );
   }
 }
+
