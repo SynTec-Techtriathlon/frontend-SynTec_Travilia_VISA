c0b6f2934e31988ea86126c70fd133d45a6a59b6
Kavishka Nimsara <127414940+kavi9919@users.noreply.github.com>
2024-09-04 19:19:25 +0530

    add theme

--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -1,12 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:visa_app/theme.dart';
 
-class MyWidget extends StatelessWidget {
-  const MyWidget({super.key});
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Flutter Demo Home Page'),
+      ),
+      body: Center(
+        child: Column(children: [
+          Text('Home page', style: headingStyle),
+        ]),
+      ),
     );
   }
 }
